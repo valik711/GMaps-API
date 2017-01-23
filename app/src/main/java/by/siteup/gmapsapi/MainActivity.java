4c5249f2ddd1ff56e0348aba82fe579cafc492d8
valik711 <androvalik@gmail.com>
2017-01-23 15:54:43 +0300

    GUI improved. Added list view that shows the whole list of places for the request

--- app/src/main/java/by/siteup/gmapsapi/MainActivity.java
+++ app/src/main/java/by/siteup/gmapsapi/MainActivity.java
@@ -1,47 +1,55 @@
 package by.siteup.gmapsapi;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
+import com.google.maps.PlacesApi;
+import com.google.maps.TextSearchRequest;
 import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    ArrayList<String> a = new ArrayList<>();
+    ArrayAdapter<String> ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //empty comment
 
+
+        final ListView places = (ListView)findViewById(R.id.listView);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button= (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText ed = (EditText) findViewById(R.id.editText);
-                GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyA-SaUHNdfLPrNTvgIhijWV09OIXmthvI4");
-                GeocodingResult[] results = new GeocodingResult[0];
-                try {
-                    results = GeocodingApi.geocode(context, String.valueOf(ed.getText()+" Минск")).language("ru").await();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
 
-                TextView myTextView = (TextView) findViewById(R.id.tv);
-                myTextView.setText(results[0].formattedAddress);
-            }
-        });
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
 
 
-
+        final ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a);
+        ad = arrayAdapter;
 
 
     }
@@ -67,5 +75,37 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+
+        EditText ed = (EditText) findViewById(R.id.editText);
+        ListView places = (ListView) findViewById(R.id.listView);
+        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyA-SaUHNdfLPrNTvgIhijWV09OIXmthvI4");
+        PlacesSearchResponse results = new PlacesSearchResponse();
+
+        try {
+            results = PlacesApi.textSearchQuery(context, String.valueOf(ed.getText() + " Минск")).language("ru").await();
+            for(PlacesSearchResult r: results.results){
+                a.add(r.formattedAddress);
+            }
+
+
+            places.setAdapter(ad);
+            InputMethodManager inputManager = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
