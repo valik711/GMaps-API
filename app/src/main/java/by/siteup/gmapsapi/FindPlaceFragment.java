4812bac97601ab8cb47e855ca5830e5023cdbdad
valik711 <androvalik@gmail.com>
2017-01-25 01:44:03 +0300

    Updated FindPlaces Fragment Added pictures and titles to layout

--- app/src/main/java/by/siteup/gmapsapi/FindPlaceFragment.java
+++ app/src/main/java/by/siteup/gmapsapi/FindPlaceFragment.java
@@ -3,7 +3,11 @@ package by.siteup.gmapsapi;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Pair;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +15,11 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.PlacesApi;
@@ -35,11 +41,11 @@ public class FindPlaceFragment extends Fragment implements View.OnClickListener{
     ArrayList<String> addresses = new ArrayList<>();
 
     TableLayout.LayoutParams rowLp = new TableLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT);
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT);
     TableRow.LayoutParams cellLp = new TableRow.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT);
+            ViewGroup.LayoutParams.WRAP_CONTENT);
 
 
     @Override
@@ -64,14 +70,46 @@ public class FindPlaceFragment extends Fragment implements View.OnClickListener{
 
             buttonsTable = (TableLayout) rootView.findViewById(R.id.butTable);
 
-            for (int r = 0; r < 8; ++r)
+            ArrayList<Pair<Integer, String>> icons = new ArrayList<>();
+            icons.add(new Pair<Integer, String>(R.drawable.transport, "Автомойки"));
+            icons.add(new Pair<Integer, String>(R.drawable.animals,"Рыбалка"));
+            icons.add(new Pair<Integer, String>(R.drawable.animals1,"Ветеринар"));
+            icons.add(new Pair<Integer, String>(R.drawable.carmechanic,"Автосервис"));
+            icons.add(new Pair<Integer, String>(R.drawable.dumbbellfortraining, "Фитнесс"));
+            icons.add(new Pair<Integer, String>(R.drawable.frontalbussilhouette,"Транспорт"));
+            icons.add(new Pair<Integer, String>(R.drawable.game,"Досуг"));
+            icons.add(new Pair<Integer, String>(R.drawable.hairdresserscissorsandcomb,"Парикмахер"));
+            icons.add(new Pair<Integer, String>(R.drawable.hamburguerdrinkwithstraw,"Фастфуд"));
+            icons.add(new Pair<Integer, String>(R.drawable.letter,"Паркинг"));
+            icons.add(new Pair<Integer, String>(R.drawable.sleepingbedsilhouette,"Гостиница"));
+            icons.add(new Pair<Integer, String>(R.drawable.shopperwithbags,"Магазин"));
+            icons.add(new Pair<Integer, String>(R.drawable.subwaysign,"Метро"));
+            icons.add(new Pair<Integer, String>(R.drawable.swimmingman,"Бассейн"));
+            icons.add(new Pair<Integer, String>(R.drawable.swinggame,"Для детей"));
+            icons.add(new Pair<Integer, String>(R.drawable.teacherreading,"Библиотека"));
+            icons.add(new Pair<Integer, String>(R.drawable.tooth,"Стоматолог"));
+            icons.add(new Pair<Integer, String>(R.drawable.trainonrailroad,"Поезд"));
+            icons.add(new Pair<Integer, String>(R.drawable.trialhammer,"Суд"));
+            icons.add(new Pair<Integer, String>(R.drawable.tshirtsilhouette,"Одежда"));
+            icons.add(new Pair<Integer, String>(R.drawable.vehicle,"Авто"));
+            icons.add(new Pair<Integer, String>(R.drawable.frontalbussilhouette,"Еще"));
+            icons.add(new Pair<Integer, String>(R.drawable.transport,"и еще"));
+            icons.add(new Pair<Integer, String>(R.drawable.animals,"и еще..."));
+
+            int n = 0;
+            for (int r = 0; r < 6; ++r)
             {
                 TableRow row = new TableRow(getActivity());
-                for (int c = 0; c < 3; ++c)
+                row.setGravity(Gravity.CENTER);
+                for (int c = 0; c < 4; ++c)
                 {
-
                     View v = inflater.inflate(R.layout.icon_button, null, false);
+                    ImageView placeIcon = (ImageView)v.findViewById(R.id.placeImage);
+                    placeIcon.setImageResource(icons.get(n).first);
+                    TextView placeText = (TextView)v.findViewById(R.id.placeText);
+                    placeText.setText(icons.get(n).second);
                     row.addView(v, cellLp);
+                    n++;
                 }
                 buttonsTable.addView(row, rowLp);
             }
